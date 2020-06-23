d3ff1425103bceb4f79b11c24dda5c54b5ac9adc
Arthur Godinho <arty1230200@gmail.com>
2020-06-23 16:16:45 -0300

    correção frontend 2

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const success = res => res
 const error = err => {
     if (401 === err.response.status) {
-        window.location = '/auth'
+        window.location = '/signin'
     }else {
         return Promise.reject(err)
     }

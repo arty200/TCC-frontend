6cd23c1583f7b785f1c87f2a32c582e07dcc6ad6
Arthur Godinho <arty1230200@gmail.com>
2020-06-23 19:42:22 -0300

    correção frontend 16

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
-const serveStatic = require('serve-static')
+//const serveStatic = require('serve-static')
 const path = require('path')
 const app = express()
 
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use(express.static(path.join(__dirname + '/dist')))
+
+app.get(/.*/, function(req, res) {
+    res.sendfile(__dirname + "/dist/index.html");
+});
 
 const port = process.env.PORT || 8080
 app.listen(port)
